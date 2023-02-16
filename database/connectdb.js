1092abbd6608982c57c3a0fa461d36690f94f208
Jhon Arlem Andrade <jhondoncan@outlook.com>
2023-02-15 23:37:52 -0500

    feat: :sparkles: Conexion a la base de datos MongoDB

---
+++ database/connectdb.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+try {
+  mongoose.set('strictQuery', false)
+  mongoose.connect(process.env.MONGODB_URI)
+  console.log('✅ MongoDB conectada')
+} catch (error) {
+  console.error(`❌ MongoDB desconectada: ${error.message}`)
+  process.exit(1)
+}
