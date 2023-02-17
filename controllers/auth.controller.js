b16b9a42a0c512fb0d05daa64240027265415ac9
Jhon Arlem Andrade <jhondoncan@outlook.com>
2023-02-16 22:57:06 -0500

    feat: :sparkles: Ahora se puede registrar el usuario

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,10 +1,19 @@
+import { Usuario } from '../models/Usuario.js'
+
+export const registro = async (req, res) => {
+  const { nombres, usuario, rol, correo, password } = req.body
+  try {
+    const nuevoUsuario = new Usuario({ nombres, usuario, rol, correo, password })
+    await nuevoUsuario.save()
+    return res.json({
+      mensaje: 'Usuario registrado'
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
 
 export const login = async (req, res) => {
   console.log(req.body)
   res.json({ mensaje: 'Inicio de sesión exitoso' })
-}
-
-export const registro = async (req, res) => {
-  console.log(req.body)
-  res.json({ mensaje: 'Registro exitoso' })
 }
