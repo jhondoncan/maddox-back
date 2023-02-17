c048827f06f6f37d9827e6ec27ac522d42c7c0bb
Jhon Arlem Andrade <jhondoncan@outlook.com>
2023-02-16 21:54:57 -0500

    feat: :sparkles: Se agregan los parametros a validar con express-validator

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,12 +1,23 @@
 import express from 'express'
+import { login, registro } from '../controllers/auth.controller.js'
+import { validacionExpress } from '../middlewares/validacionExpress.js'
+import { body } from 'express-validator'
+
 const router = express.Router()
 
-router.post('/login', (req, res) => {
-  res.json({ mensaje: 'Iniciar sesión en Maddox' })
-})
+router.post('/login', [
+  body('usuario', 'El usuario es obligatorio').trim().notEmpty().escape(),
+  body('password', 'La contraseña es obligatoria').trim().notEmpty().escape(),
+  body('password', 'La contraseña debe tener al menos 6 caracteres').trim().isLength({ min: 6 }).escape()
+], validacionExpress, login)
 
-router.post('/registro', (req, res) => {
-  res.json({ mensaje: 'Registrarse en Maddox' })
-})
+router.post('/registro', [
+  body('nombres', 'El nombre es obligatorio').trim().notEmpty().escape(),
+  body('usuario', 'El nombre de usuario es obligatorio').trim().notEmpty().escape(),
+  body('rol', 'El rol es obligatorio').trim().notEmpty().escape(),
+  body('correo', 'El correo es obligatorio').trim().isEmail().normalizeEmail().notEmpty().escape(),
+  body('password', 'La contraseña es obligatoria').trim().notEmpty().escape(),
+  body('password', 'La contraseña debe tener al menos 6 caracteres').trim().isLength({ min: 6 }).notEmpty().escape()
+], validacionExpress, registro)
 
 export default router
