8a0337bbb97370283d9ca28a59f861b8fb888961
Jhon Arlem Andrade <jhondoncan@outlook.com>
2023-02-17 17:19:29 -0500

    feat: :sparkles: Se crea metodo para generar token y refrecar token

---
+++ helpers/generarToken.js
@@ -0,0 +1,17 @@
+import jwt from 'jsonwebtoken'
+
+export const generarToken = (usuario) => {
+    const token = jwt.sign({ id: usuario.id, nombres: usuario.nombres, rol: usuario.rol }, process.env.JWT_SECRET, { expiresIn: '30m' })
+    return token
+}
+
+export const generarRefreshToken = (usuario) => {
+    const refreshToken = jwt.sign({ id: usuario.id, nombres: usuario.nombres, rol: usuario.rol }, process.env.JWT_SECRET, { expiresIn: '1h' })
+
+    // eslint-disable-next-line no-undef
+    res.cookie('refreshToken', refreshToken, {
+        httpOnly: true,
+        secure: !process.env.MODO === 'development',
+        expires: new Date(Date.now() + (60 * 60))
+    })
+}
