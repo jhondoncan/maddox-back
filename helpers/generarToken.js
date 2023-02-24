63a9305fd32455a3d8e880b45ff5bd5e8c0e28f6
Jhon Arlem Andrade <jhondoncan@outlook.com>
2023-02-24 17:20:55 -0500

    fix: :bug: Se cambia la expiracion al token y refresh

--- helpers/generarToken.js
+++ helpers/generarToken.js
@@ -1,12 +1,12 @@
 import jwt from 'jsonwebtoken'
 
 export const generarToken = (usuario) => {
-  const token = jwt.sign({ id: usuario.id, nombres: usuario.nombres, rol: usuario.rol }, process.env.JWT_SECRET, { expiresIn: '30m' })
+  const token = jwt.sign({ id: usuario.id, nombres: usuario.nombres, rol: usuario.rol }, process.env.JWT_SECRET, { expiresIn: '1h' })
   return token
 }
 
 export const generarRefreshToken = (usuario) => {
-  const refreshToken = jwt.sign({ id: usuario.id, nombres: usuario.nombres, rol: usuario.rol }, process.env.JWT_SECRET, { expiresIn: '1h' })
+  const refreshToken = jwt.sign({ id: usuario.id, nombres: usuario.nombres, rol: usuario.rol }, process.env.JWT_SECRET, { expiresIn: '15m' })
   // eslint-disable-next-line no-undef
   res.cookie('refreshToken', refreshToken, {
     httpOnly: true,
